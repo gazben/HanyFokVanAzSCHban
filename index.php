597715dfc9827f3ed29059454e8ef4e0d0a37e03
Gujgiczer Máté <gujgi@wearebreak.com>
2014-05-10 22:06:39 +0200

    bullshit

--- index.php
+++ index.php
@@ -5,18 +5,40 @@
     <link href="//netdna.bootstrapcdn.com/bootstrap/3.1.1/css/bootstrap.min.css" rel="stylesheet">
 </head>
 <body>
+<div class="jumbotron">
     <h1>Ennyi fok uralkodik jelenleg az SCH710-es szobájában</h1>
-    <br />
+
 
 <div id="php">
     <?php
-		include("datas.php");
+
+            <div class="progress">
+              <div class="progress-bar progress-bar-warning" role="progressbar" aria-valuenow="\".$_GET["temp"].\"" aria-valuemin="0" aria-valuemax="100" style="width: \".$_GET["temp"].\"">
+                Temp: ".$_GET["temp"]."
+              </div>
+            </div>
+
+
+            <div class="progress">
+              <div class="progress-bar" role="progressbar" aria-valuenow="\".$_GET["humidity"].\"" aria-valuemin="0" aria-valuemax="100" style="width: \".$_GET["humidity"].\";">
+                Humidity: ".$_GET["humidity"]."
+              </div>
+            </div>
+
+            <p> Measure time: ".date("F j, Y, g:i a")." </p>
+
     ?>
 </div>
+</div>
 
-        Opre feladat is live! made by Balu&&Gazben
-<br />
-        On Bootsrtap
+<div class="panel panel-default" style="text-align: center; bottom: 20px; position: absolute;">
+  <div class="panel-body">
+    Opre feladat is live! made by Balu&&Gazben
+
+  </div>
+</div>
+
+
 </body>
 
 </html>
--- set.php
+++ set.php
@@ -10,4 +10,12 @@ Humidity: ".$_GET["humidity"]."%
 Measure time:  ".date("F j, Y, g:i a")."
 <br >
 <br />");
+
+file_put_contents("data.json","{
+\"temp\": ".$_GET["temp"].",
+\"hum\": ".$_GET["humidity"].",
+\"time\": \"".date("F j, Y, g:i a")."\"
+}");
+
+
 ?>
