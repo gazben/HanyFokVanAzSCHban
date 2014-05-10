5f88dcef519f73438a711cd0235c3ba74a44aa2f
Gazben <bencegazder@gmail.com>
2014-05-10 20:58:50 +0200

    Berci művészkedett osszátok be

---
+++ datas.php
@@ -0,0 +1,7 @@
+Temp: 26.28 C
+<br />
+Humidity: 24.61%
+<br />
+Measure time:  May 10, 2014, 8:58 pm
+<br >
+<br />
--- set.php
+++ set.php
@@ -1,32 +1,13 @@
 <?php
-
 file_put_contents("data.txt","Temp: ".$_GET["temp"]."
  Humidity: ".$_GET["humidity"]."
  Measure time:  ".date("F j, Y, g:i a"));
-file_put_contents("index.html",
-
-"<html>
-<head>
-<meta charset= "UTF8"/>
-<title>Hány fok van az 710-ben?</title>
-<link href="//netdna.bootstrapcdn.com/bootstrap/3.1.1/css/bootstrap.min.css" rel="stylesheet">
-</head>
-<body>
-<h1>Ennyi fok uralkodik jelenleg az SCH710-es szobájában</h1>
+ 
+file_put_contents("datas.php", "Temp: ".$_GET["temp"]." C
 <br />
-Temp: ".$_GET["temp"]."
-<br />
-Humidity: ".$_GET["humidity"]."
+Humidity: ".$_GET["humidity"]."%
 <br />
 Measure time:  ".date("F j, Y, g:i a")."
 <br >
-<br />
-<br >
-Opre feladat is live! made by Balu&&Gazben
-<br />
-
-<script src="//netdna.bootstrapcdn.com/bootstrap/3.1.1/js/bootstrap.min.js"></script>
-</body>
-</html>");
-
+<br />");
 ?>
