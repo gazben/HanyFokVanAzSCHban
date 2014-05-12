1a32fbc62dc36ace8f85a871ada28133cfa1fd41
netpok <netpok@gmail.com>
2014-05-12 22:59:46 +0200

    Signed-off-by: netpok <netpok@gmail.com>

--- datas.php
+++
@@ -1,7 +0,0 @@
-Temp: 26.28 C
-<br />
-Humidity: 24.61%
-<br />
-Measure time:  May 10, 2014, 8:58 pm
-<br >
-<br />
--- set.php
+++ set.php
@@ -1,20 +1,12 @@
 <?php
 file_put_contents("data.txt","Temp: ".$_GET["temp"]."
  Humidity: ".$_GET["humidity"]."
- Measure time:  ".date("F j, Y, g:i a"));
- 
-file_put_contents("datas.php", "Temp: ".$_GET["temp"]." C
-<br />
-Humidity: ".$_GET["humidity"]."%
-<br />
-Measure time:  ".date("F j, Y, g:i a")."
-<br >
-<br />");
+ Measure time:  ".date("F j, Y, H:i"));
 
 file_put_contents("data.json","{
 \"temp\": ".$_GET["temp"].",
 \"hum\": ".$_GET["humidity"].",
-\"time\": \"".date("F j, Y, g:i a")."\"
+\"time\": \"".date("F j, Y, H:i")."\"
 }");
 
 
